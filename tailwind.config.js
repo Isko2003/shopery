74b590a36afe642aa257934b601fdb5e55629fd6
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-24 14:11:16 +0400

    adding custom colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        soft_primary: "#84D187",
+        primary: "#00B207",
+        hard_primary: "#2C742F",
+        warning: "#FF8A00",
+        danger: "#EA4B48",
+        white:"#FFFFFF",
+      }
+    },
   },
   plugins: [],
 }
