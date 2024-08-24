010fd397adfb4d9572e7c34520e8467fd705766c
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-24 15:21:43 +0400

    creatingn env file and supabase client

---
+++ supabaseClient.js
@@ -0,0 +1,7 @@
+const supabaseKey = import.meta.env.VITE_DATABASE_API_KEY;
+const supabaseUrl = import.meta.env.VITE_DATABASE_URL;
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
