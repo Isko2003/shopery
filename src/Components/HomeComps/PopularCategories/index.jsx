d16c7925c4beb9b1d147031ce4c36fa153517721
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-26 00:15:44 +0400

    replacing a piece of code with category card component

--- src/Components/HomeComps/PopularCategories/index.jsx
+++ src/Components/HomeComps/PopularCategories/index.jsx
@@ -1,20 +1,18 @@
 import { popularCategories } from "../../../Data/data";
 import MainButton from "../../Common/MainButton"
 import { FaArrowRight } from "react-icons/fa6";
+import CategoryCard from "./CategoryCard";
 
 const PopularCategoriesH = () => {
     return (
         <section className="section" >
-            <div className="flex justify-between items-center">
+            <div className="justifyBetweenItems">
                 <h3 className="font-semibold text-3xl">Popular Categories</h3>
                 <MainButton classNames="btn-1" title="View All" icon={<FaArrowRight />} url="" />
             </div>
             <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3 my-5">
-                {popularCategories.map(({ id, title, imgUrl }) => {
-                    return <div className="categoryCard" key={id}>
-                        <img src={imgUrl} alt={title} />
-                        <h5 className="my-2 text-lg">{title}</h5>
-                    </div>
+                {popularCategories.map((category) => {
+                    return <CategoryCard key={category.id} data={category} />
                 })}
             </div>
         </section>
