0db6d11364f614e3804516121e0074b514744e13
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-25 21:54:30 +0400

    adding some static data

---
+++ src/Data/data.js
@@ -0,0 +1,98 @@
+export const featuresCards = [
+    {
+        id: 0,
+        title: "Free Shipping",
+        desc: "Free shipping on all your order",
+        imgUrl: "/delivery-truck_i.svg"
+    }, {
+        id: 1,
+        title: "Customer Support 24/7",
+        desc: "Instant access to Support",
+        imgUrl: "/headphones_i.svg"
+    }, {
+        id: 2,
+        title: "100% Secure Payment",
+        desc: "We ensure your money is save",
+        imgUrl: "/magazine_i.svg"
+    }, {
+        id: 3,
+        title: "Money-Back Guarantee",
+        desc: "30 Days Money-Back Guarantee",
+        imgUrl: "/box_i.svg"
+    }
+]
+
+export const popularCategories = [
+    {
+        id: 0,
+        title: "Fresh Fruit",
+        imgUrl: "/fresh_fruit_C.png",
+        url: ""
+    },
+    {
+        id: 1,
+        title: "Fresh Vegetables",
+        imgUrl: "/vegetables_ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Meat & Fish",
+        imgUrl: "/meatandfish_ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Snacks",
+        imgUrl: "/snacks_C.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Beverages",
+        imgUrl: "/beverages_c.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Beauty & Health",
+        imgUrl: "/beuaty_healthy_ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Bread & Bakery",
+        imgUrl: "/bread_bakery_ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Baking Needs",
+        imgUrl: "/baking_needs_Ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Cooking",
+        imgUrl: "/cooking_ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Diabetic Food",
+        imgUrl: "/diabetic_foods_ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Dish Detergents",
+        imgUrl: "/dish_detergents_Ca.png",
+        url: ""
+    },
+    {
+        id: 0,
+        title: "Oil",
+        imgUrl: "/oil_ca.png",
+        url: ""
+    },
+] 
