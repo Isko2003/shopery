bb8bd43e6cb093a90b01c003e5c97aabafaf073d
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-08-29 18:03:17 +0400

    adding new components to the shop page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import Breadcrumb from '../Components/Common/Breadcrumb'
 import PageContainer from '../Components/Common/PageContainer'
+import BannerShop from '../Components/ShopComps/BannerShop'
 
+const shopLinks = [
+  { id: 0, title: "Categories", url: "/categories" },
+  { id: 1, title: "Vegetables", url: "/categories/vegetables" },
+  
+]
 const Shop = () => {
   return (
     <div>
       <PageContainer>
+        <Breadcrumb links={shopLinks} />
+        <BannerShop/>
         Shop
       </PageContainer>
     </div>
