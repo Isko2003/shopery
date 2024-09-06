e926a5daeb1f23bb44e3bbe2f9bcdda368ac5ed3
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-09-06 16:16:41 +0400

    adding prop for cathcing the component is come from the filter section or not

--- src/Components/Common/SearchInput.jsx
+++ src/Components/Common/SearchInput.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import '../../Styles/header.css'
 import { IoSearchSharp } from "react-icons/io5";
 
-export const Button = () => {
+export const Button = ({ isFilter }) => {
   return (
-    <div className='hidden md:flex justify-center items-center'>
-    <div className='header-input'>
-      <input type="text" placeholder='Search' className='search-input br-6'/>
-      <span className='search-icon'><IoSearchSharp className='icon'/></span>
+
+    <div className={`hidden md:flex items-center ${!isFilter && "justify-center" }`}>
+      <div className='header-input'>
+        <input type="text" placeholder='Search' className='search-input br-6' />
+        <span className='search-icon'><IoSearchSharp className='icon' /></span>
+      </div>
+      {
+      !isFilter && <button className='search-btn br-6 hover:transition-all duration-500 ease'>Search</button>
+      }
     </div>
-    <button className='search-btn br-6 hover:transition-all duration-500 ease'>Search</button>
-  </div>
   )
 }
 
