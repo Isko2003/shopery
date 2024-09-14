4ce3d7336d72750c8197947ec0aaf8606d2cc0e5
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-09-14 15:50:46 +0400

    provide queryclient

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Routing from './Routing/Routing'
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import supabase from '../supabaseClient';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient()
 function App() {
   console.log(supabase)
   return (
+    <QueryClientProvider client={queryClient}>
       <Routing />
+    </QueryClientProvider>
   )
 }
 
