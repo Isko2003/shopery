6e637393ff36ccf23de0e4e4d5a309f421882f07
Isko2003 <theazeisko2003@gmail.com>
2024-10-05 13:16:01 +0400

    last deploy

--- src/Services/api.jsx
+++ src/Services/api.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import supabase from "../../supabaseClient";
 import PageContainer from "../Components/Common/PageContainer";
+import { data } from "autoprefixer";
 
 const Api = () => {
+  // .insert([
+  //     {
+  //       name: "Surjapur Mango",
+  //       category: "Vegetables",
+  //       price: 34,
+  //       image_url:
+  //         "https://mmimwhdzlgveordweoop.supabase.co/storage/v1/object/public/Images/mango.png?t=2024-09-14T09%3A40%3A36.215Z",
+  //     },
+  //   ]);
   const handleInsert = async () => {
-    const { data, error } = await supabase.rpc("pg_execute", {
-      query: `
-        ALTER TABLE Shopery_Data
-        ADD COLUMN created_at timestamp;
-        `,
-    });
+    const { data, error } = await supabase.from("Shopery_Data").select("*");
     if (error) {
-      console.error("Error inserting column: ", error);
+      console.error("Error fetching data: ", error);
     } else {
-      console.log("Column Added: ", data);
+      console.log("Fetched Data: ", data);
     }
   };
 
@@ -21,7 +26,7 @@ const Api = () => {
     <PageContainer>
       <div>
         <button onClick={handleInsert} className="border 1px solid">
-          Insert Column
+          Show Data
         </button>
       </div>
     </PageContainer>
