85d5e525734c8b8cfa12976e0d7b6183e6d0e364
Isko2003 <theazeisko2003@gmail.com>
2024-10-05 13:20:29 +0400

    resolved import error

--- src/Components/ClientCard.jsx
+++ src/Components/ClientCard.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
+import React from "react";
 import { RiDoubleQuotesR } from "react-icons/ri";
-import robert from '../assets/imgs/robert.png';
+import robert from "../assets/imgs/robert.png";
 import { FaStar } from "react-icons/fa";
 
 const ClientCard = ({ client }) => {
-
-    const { img, text, clientName, position } = client;
+  const { img, text, clientName, position } = client;
 
   return (
-    <div className='bg-[white] flex justify-between client-box'>
-            <div className='w-[350px] h-[35vh]'>
-                <div className='quotes'>
-                    <RiDoubleQuotesR className='text-4xl text-[#649e66]'/>
-                </div>
-                <div className='client-text'>
-                    <p className='w-[320px] py-4 text-sm text-left'>{text}</p>
-                </div>
-                <div className='client-info flex justify-between items-center'>
-                    <div className='flex gap-4'>
-                        <div className='rounded-full'>
-                            <img src={img} alt="client" />
-                        </div>
-                        <div className='flex flex-col justify-center'>
-                            <h3>{clientName}</h3>
-                            <p>{position}</p>
-                        </div>
-                    </div>
-                    <div className='stars flex pb-1'>
-                        <span><FaStar className='text-yellow-400'/></span>
-                        <span><FaStar className='text-yellow-400'/></span>
-                        <span><FaStar className='text-yellow-400'/></span>
-                        <span><FaStar className='text-yellow-400'/></span>
-                        <span><FaStar className='text-yellow-400' /></span>
-                    </div>
-                </div>
+    <div className="bg-[white] flex justify-between client-box">
+      <div className="w-[350px] h-[35vh]">
+        <div className="quotes">
+          <RiDoubleQuotesR className="text-4xl text-[#649e66]" />
+        </div>
+        <div className="client-text">
+          <p className="w-[320px] py-4 text-sm text-left">{text}</p>
+        </div>
+        <div className="client-info flex justify-between items-center">
+          <div className="flex gap-4">
+            <div className="rounded-full">
+              <img src={img} alt="client" />
             </div>
+            <div className="flex flex-col justify-center">
+              <h3>{clientName}</h3>
+              <p>{position}</p>
+            </div>
+          </div>
+          <div className="stars flex pb-1">
+            <span>
+              <FaStar className="text-yellow-400" />
+            </span>
+            <span>
+              <FaStar className="text-yellow-400" />
+            </span>
+            <span>
+              <FaStar className="text-yellow-400" />
+            </span>
+            <span>
+              <FaStar className="text-yellow-400" />
+            </span>
+            <span>
+              <FaStar className="text-yellow-400" />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ClientCard
+export default ClientCard;
--- src/Components/ClientTestimonials.jsx
+++ src/Components/ClientTestimonials.jsx
@@ -1,60 +1,62 @@
-import React from 'react'
+import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import '../Styles/swiper.css';
-import { Autoplay } from 'swiper/modules';
-import { clientData } from '../Data/clientTestimonials';
-import ClientCard from './clientCard';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../Styles/swiper.css";
+import { Autoplay } from "swiper/modules";
+import { clientData } from "../Data/clientTestimonials";
+import ClientCard from "./ClientCard";
 
 const ClientTestimonials = () => {
   return (
-    <div className='w-[100%] mx-auto pb-20 bg-[#f2f2f2] py-6'>
-        <div className='flex justify-between pt-3 pb-4 items-center px-0 sm:px-24 '>
-            <h1 className='text-3xl font-bold'>Client Testimonials</h1>
-            <div className='flex gap-5'>
-                <span className='bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center'><FaArrowLeft className='cursor-pointer'/></span>
-                <span className='bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center'><FaArrowRight className='cursor-pointer'/></span>
-            </div>
+    <div className="w-[100%] mx-auto pb-20 bg-[#f2f2f2] py-6">
+      <div className="flex justify-between pt-3 pb-4 items-center px-0 sm:px-24 ">
+        <h1 className="text-3xl font-bold">Client Testimonials</h1>
+        <div className="flex gap-5">
+          <span className="bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center">
+            <FaArrowLeft className="cursor-pointer" />
+          </span>
+          <span className="bg-white rounded-full w-[40px] h-[40px] flex items-center justify-center">
+            <FaArrowRight className="cursor-pointer" />
+          </span>
         </div>
-        <div className='flex justify-between mx-auto w-[85%]'>
-            <Swiper
-            spaceBetween={30}
-            centeredSlides={true}
-            slidesPerView={3}
-            loop={true}
-            breakpoints={{
-                320: {
-                    slidesPerView: 1,
-                },
-                768: {
-                    slidesPerView: 2,
-                },
-                1280: {
-                    slidesPerView: 3,
-                }
-            }}
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false
-            }}
-            modules={[Autoplay]}
-            className='mySwiper'
-            >
-                {
-                    clientData.map((item,index) => (
-                        <SwiperSlide key={index}>
-                            <ClientCard client={item} key={index} />
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
-        </div>
+      </div>
+      <div className="flex justify-between mx-auto w-[85%]">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          slidesPerView={3}
+          loop={true}
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1280: {
+              slidesPerView: 3,
+            },
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          {clientData.map((item, index) => (
+            <SwiperSlide key={index}>
+              <ClientCard client={item} key={index} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ClientTestimonials
+export default ClientTestimonials;
